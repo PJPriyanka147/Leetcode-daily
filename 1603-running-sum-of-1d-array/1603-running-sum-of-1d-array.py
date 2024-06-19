01b085d3d2b52e806b7805d70a38a6ec524e3469
Priyanka Jatav <108455264+PJPriyanka147@users.noreply.github.com>
2024-06-19 22:06:22 +0530

    Time: 42 ms (56.63%), Space: 16.7 MB (37.27%) - LeetHub

--- 1603-running-sum-of-1d-array/1603-running-sum-of-1d-array.py
+++ 1603-running-sum-of-1d-array/1603-running-sum-of-1d-array.py
@@ -1,10 +1,7 @@
 class Solution:
     def runningSum(self, nums: List[int]) -> List[int]:
-        
-        #create a new list that contains the same elements as nums but is a         different object in memory.
-        running_sum = nums[:]
 
         for i in range(1, len(nums)):
-            running_sum[i] = running_sum[i - 1] + running_sum[i]
+            nums[i] = nums[i - 1] + nums[i]
         
-        return running_sum
+        return nums
