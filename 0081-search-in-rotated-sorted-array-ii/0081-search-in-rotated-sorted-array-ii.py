99b52d2f81f617f5d7a8108696a16159f9bc7b59
Priyanka Jatav <108455264+PJPriyanka147@users.noreply.github.com>
2024-07-15 16:46:25 +0530

    Time: 53 ms (54.25%), Space: 17.1 MB (29.75%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.py
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.py
@@ -1,6 +1,27 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> bool:
-        for num in nums:
-            if num == target:
+        low, high = 0, len(nums) - 1
+
+        while low <= high:
+            mid = low + (high - low) // 2
+        
+            if nums[mid] == target: 
                 return True
+        
+            # If we encounter duplicates, just move the pointers
+            if nums[low] == nums[mid] == nums[high]:
+                low += 1
+                high -= 1
+        
+            elif nums[low] <= nums[mid]:
+                if nums[low] <= target < nums[mid]:
+                    high = mid - 1
+                else: 
+                    low = mid + 1
+            else:
+                if nums[mid] < target <= nums[high]:
+                    low = mid + 1
+                else:
+                    high = mid - 1
+    
         return False
